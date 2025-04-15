fd92d53b8a204d174755ec3ee15ec8dc2bb8c269
Danyh9 <444201030@student.ksu.edu.sa>
2025-04-15 23:34:02 +0300

    Update Allocation.java

--- OS_Assignment2/Allocation.java
+++ OS_Assignment2/Allocation.java
@@ -11,15 +11,21 @@ import java.util.List;
 public class Allocation {
 
     public void FirstFit(String pID,int pSize,List<Block> memoryBlocks){ //Allocate the first available block that is large enough.
-        int firstFit=-1;
-        for(Block block : memoryBlocks){
-           if(!block.isAllocated){
-                if(block.blockSize>=pSize){
-                    firstFit=memoryBlocks.indexOf(block);
-                    break;
-                }
-           }
+        int firstFit = -1;
+    for (Block block : memoryBlocks) {
+        if (!block.isAllocated) {
+            if (block.blockSize >= pSize) {
+                firstFit = memoryBlocks.indexOf(block);
+                break;
+            }
         }
+    }
+
+    // Check if we found a suitable block
+    if (firstFit == -1) {
+        System.out.println("Error: No suitable block found for process " + pID);
+        return;
+    }
         Block firstFitedBlock=memoryBlocks.get(firstFit);
         firstFitedBlock.isAllocated=true;
         firstFitedBlock.processID=pID;
@@ -32,17 +38,22 @@ public class Allocation {
 
     }
     public void BestFit(String pID,int pSize,List<Block> memoryBlocks){  // Allocate the smallest block that is large enough.
-        int bestFit=-1;
-        for(Block block : memoryBlocks){
-            if(block.blockSize>=pSize){
-                if(bestFit==-1){
-                    bestFit=memoryBlocks.indexOf(block);
-                }
-                else if(memoryBlocks.get(bestFit).blockSize>block.blockSize){
-                    bestFit=memoryBlocks.indexOf(block);
-                }
+         int bestFit = -1;
+    for (Block block : memoryBlocks) {
+        if (block.blockSize >= pSize) {
+            if (bestFit == -1) {
+                bestFit = memoryBlocks.indexOf(block);
+            } else if (memoryBlocks.get(bestFit).blockSize > block.blockSize) {
+                bestFit = memoryBlocks.indexOf(block);
             }
         }
+    }
+
+    // Check if we found a suitable block
+    if (bestFit == -1) {
+        System.out.println("Error: No suitable block found for process " + pID);
+        return;
+    }
         Block bestFitedBlock=memoryBlocks.get(bestFit);
         bestFitedBlock.isAllocated=true;
         bestFitedBlock.processID=pID;
@@ -56,17 +67,22 @@ public class Allocation {
 
     }
     public void WorstFit(String pID,int pSize,List<Block> memoryBlocks){
-        int worstFit=-1;
-        for(Block block : memoryBlocks){
-            if(block.blockSize>=pSize){
-                if(worstFit==-1){
-                    worstFit=memoryBlocks.indexOf(block);
-                }
-                else if(memoryBlocks.get(worstFit).blockSize<block.blockSize){
-                    worstFit=memoryBlocks.indexOf(block);
-                }
+         int worstFit = -1;
+    for (Block block : memoryBlocks) {
+        if (block.blockSize >= pSize) {
+            if (worstFit == -1) {
+                worstFit = memoryBlocks.indexOf(block);
+            } else if (memoryBlocks.get(worstFit).blockSize < block.blockSize) {
+                worstFit = memoryBlocks.indexOf(block);
             }
         }
+    }
+
+    // Check if we found a suitable block
+    if (worstFit == -1) {
+        System.out.println("Error: No suitable block found for process " + pID);
+        return;
+    }
         Block worstFittedBlock=memoryBlocks.get(worstFit);
         worstFittedBlock.isAllocated=true;
         worstFittedBlock.processID=pID;
